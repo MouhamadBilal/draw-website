28794c109d18f3f9df2fe5c03f86610dbe36b616
salman <mb.salman95140@gmail.com>
2022-11-22 11:51:48 +0100

    build: Manage form submission

--- src/Controller/DrawController.php
+++ src/Controller/DrawController.php
@@ -3,32 +3,52 @@
 namespace App\Controller;
 
 use App\Repository\DrawRepository;
-use http\Env\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Draw;
 use App\Form\DrawFormType;
 use Doctrine\ORM;
+use Doctrine\ORM\EntityManagerInterface;
 
 use Twig\Environment;
 
 class DrawController extends AbstractController
 {
-    #[Route('/draw', name: 'app_draw')]
-    public function index(Environment $twig ): Response
+
+    private EntityManagerInterface $entityManager;
+    private Environment $twig;
+    public function __construct(Environment $twig, EntityManagerInterface $entityManager )
     {
+        $this->entityManager = $entityManager;
+        $this->twig= $twig;
+    }
 
-
+    #[Route('/draw', name: 'app_draw')]
+    public function index(Environment $twig, Request $request ): Response
+    {
 
         $draw = new Draw();
         $form = $this->createForm(DrawFormType::class, $draw);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid())
+        {
+            $this->entityManager->persist($draw);
+            $this->entityManager->flush();
+
+            return $this->redirectToRoute('/draw');
+        }
 
         return new Response($twig->render('draw/index.html.twig', [
             'draw' => $draw,
             'controller_name' => 'Draw-Website',
             'draw_form' => $form->createView()
         ]));
+
+
 
     }
 
